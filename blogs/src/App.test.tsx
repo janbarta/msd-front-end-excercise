7fb3c03cf0406343341f50001829e4faa209c0d0
alphasr <nilaybaranwal@gmail.com>
2021-03-21 14:24:03 +0100

    added few tests and manual

--- blogs/src/App.test.tsx
+++ blogs/src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import ReactDOM from "react-dom";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { PublishedPost } from "./pages/PublishedPost";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+describe("Renders post list", () => {
+  // Tests will go here using `it` blocks
+  it("display list of posts", () => {
+    const wrapper = mount(<PublishedPost />);
+    expect(wrapper.find(".post")).toHaveLength(2);
+  });
+});
+
+//API tests can be implemented by mocking the useEffect and fetch operations.
